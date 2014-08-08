c271f0b8b201c46517980830651652213d1afa6d
MichelSousa <michel-damasceno@hotmail.com>
2014-08-08 01:13:34 -0300

    deploy header

---
+++ functions.php
@@ -0,0 +1,227 @@
+<?php ob_start(); 
+if ( function_exists( 'add_theme_support' ) ) { 
+add_theme_support( 'post-thumbnails' );
+set_post_thumbnail_size( 150, 150, true ); // default Post Thumbnail dimensions (cropped)
+
+
+add_image_size( 'blog', null,null); 
+add_image_size( 'cardapio', 150, 150 ); 
+add_image_size( 'slide', 1005, 495 ); 
+add_image_size( 'social', 40, 40 ); 
+add_image_size( 'social-mini', 60, 60 ); 
+add_image_size( 'social-medium', 160, 45 ); 
+add_image_size( 'blog-single', 476,215);
+add_image_size( 'minuatura', 479,215); 
+
+if ( function_exists( 'register_nav_menu' ) ) {
+register_nav_menu( 'primary', 'Este é meu primeiro menu' );
+
+}
+
+
+function excerpt($limit) {
+$excerpt = explode(' ', get_the_excerpt(), $limit);
+if (count($excerpt)>=$limit) {
+array_pop($excerpt);
+$excerpt = implode(" ",$excerpt).'...';
+} else {
+$excerpt = implode(" ",$excerpt);
+}
+$excerpt = preg_replace('`\[[^\]]*\]`','',$excerpt);
+return $excerpt;
+}
+
+
+}
+
+
+
+add_action( 'wp_head', 'sushiai_css_customizer' );
+
+add_action( 'customize_register', 'gamesquare_customize_register' );
+
+wp_register_script('jquery', get_bloginfo('template_url').'/js/jquery.js');
+wp_enqueue_script('jquery');
+
+//====================== bootstrap
+ wp_register_script('bootstrap', get_bloginfo('template_url').'/js/bootstrap.min.js');
+ wp_enqueue_script('bootstrap');
+
+
+//====================== bootstrap-collapse
+ wp_register_script('bootstrap-collapse', get_bloginfo('template_url').'/js/bootstrap-collapse.js');
+ wp_enqueue_script('bootstrap-collapse');
+
+//====================== bootstrap-dropdown
+ wp_register_script('bootstrap-dropdown', get_bloginfo('template_url').'/js/bootstrap-dropdown.js');
+ wp_enqueue_script('bootstrap-dropdown');
+
+//====================== bootstrap-dropdown
+ wp_register_script('bootstrap-dropdown', get_bloginfo('template_url').'/js/bootstrap-dropdown.js');
+ wp_enqueue_script('bootstrap-dropdown');
+
+//====================== easing
+ wp_register_script('easing', get_bloginfo('template_url').'/js/jquery.easing.min.js');
+ wp_enqueue_script('easing');
+
+
+/* Desenvolvido por Michel Damasceno */
+
+function gamesquare_customize_register( $wp_customize )
+{
+    
+
+//===================== Upload do logotipo
+
+$wp_customize->add_section( 'theme_image' , array(
+     'title'     => __( 'Logo', 'theme' ),
+      'description'   => 'Modifique o logo',
+) );
+
+$wp_customize->add_setting( 'theme_logo_image' , array(
+    'default'     => 'http://preview.vicomercial.com.br/sushiai/wp-content/themes/sushiai/img/logo.png',
+) );
+
+$wp_customize->add_control( new WP_Customize_Image_Control( $wp_customize, 'theme_logo_image', array(
+	'label'        => __( 'Logo', 'theme' ),
+	'section'    => 'theme_image',
+	'settings'   => 'theme_logo_image',
+) ) );
+
+
+//===================== Upload do logo cardápio
+
+$wp_customize->add_section( 'sushiai_image_cardapio' , array(
+     'title'     => __( 'Image cardápio', 'sushiai' ),
+      'description'   => 'Modifique o logo da seção cardápio',
+) );
+
+$wp_customize->add_setting( 'logo_image_cardapio' , array(
+    'default'     => 'http://preview.vicomercial.com.br/sushiai/wp-content/themes/sushiai/img/logo.png',
+) );
+
+$wp_customize->add_control( new WP_Customize_Image_Control( $wp_customize, 'logo_image_cardapio', array(
+	'label'        => __( 'Logo Cardapio', 'sushiai' ),
+	'section'    => 'sushiai_image_cardapio',
+	'settings'   => 'logo_image_cardapio',
+) ) );
+
+
+//===================== Url like facebook box
+
+$wp_customize->add_section( 'sushiai_box_facebook' , array(
+     'title'     => __( 'Url like facebook box', 'sushiai' ),
+      'description'   => 'URL  like box facebook',
+) );
+
+$wp_customize->add_setting( 'ur_like_facebook' , array(
+    'default'     => 'https://www.facebook.com/FacebookDevelopers',
+) );
+
+$wp_customize->add_control('url_like', array(
+	'label'        => __( 'url', 'sushiai' ),
+	'section'    => 'sushiai_box_facebook',
+	'settings'   => 'ur_like_facebook',
+)  );
+
+//===================== Número de telefone
+
+$wp_customize->add_section( 'sushiai_fone' , array(
+     'title'     => __( 'Número de telefone', 'sushiai' ),
+      'description'   => 'Digite o número de telefone',
+) );
+
+$wp_customize->add_setting( 'text_fone' , array(
+    'default'     => '(41) 3672-4116',
+) );
+
+$wp_customize->add_control('sushiai_fone_text', array(
+  'label'        => __( 'url', 'sushiai' ),
+  'section'    => 'sushiai_fone',
+  'settings'   => 'text_fone',
+)  );
+
+
+//===================== Endereço
+
+$wp_customize->add_section( 'sushiai_endereco' , array(
+     'title'     => __( 'Endereço', 'sushiai' ),
+      'description'   => 'Digite o seu endereço',
+) );
+
+$wp_customize->add_setting( 'text_endereco' , array(
+    'default'     => 'Rua Lucia Madalena Strapassoni, 154 | Sala 5, Quatro Barras',
+) );
+
+$wp_customize->add_control('sushiai_endereco_text', array(
+  'label'        => __( 'url', 'sushiai' ),
+  'section'    => 'sushiai_endereco',
+  'settings'   => 'text_endereco',
+)  );
+
+}
+
+
+function sushiai_css_customizer()
+{
+
+	?>
+
+ <style type="text/css">
+     body{ background-color:#<?php echo get_theme_mod('background_color') ; ?>  }
+ </style>
+
+	<?php
+}
+
+
+
+
+// Registrar áreas de widgets
+function theme_widgets_init() {
+ // Área 1
+ register_sidebar( array (
+ 'name' => 'Primary Widget Area',
+ 'id' => 'primary_widget_area',
+
+  ) );
+ 
+
+ register_sidebar( array (
+ 'name' => 'Secondary Widget Area',
+ 'id' => 'secondary_widget_area',
+ 
+  ) );
+
+
+} // end theme_widgets_init
+ 
+add_action( 'init', 'theme_widgets_init' );
+
+
+
+if ( isset( $_GET['activated'] ) ) {
+ update_option( 'sidebars_widgets', $preset_widgets );
+}
+// update_option( 'sidebars_widgets', NULL );
+
+// Verificar widgets nas áreas de widgets
+function is_sidebar_active( $index ){
+  global $wp_registered_sidebars;
+ 
+  $widgetcolums = wp_get_sidebars_widgets();
+ 
+  if ($widgetcolums[$index]) return true;
+ 
+ return false;
+} 
+
+
+
+
+?>  
+
+
+	 
+	 
+	
---
+++ header.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>Sphera</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="<?php bloginfo("template_url")?>/css/bootstrap.min.css" rel="stylesheet">
+
+    <link href="<?php bloginfo("template_url")?>/css/bootstrap.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+
+    <link href="<?php bloginfo("template_url")?>/css/carousel.css" rel="stylesheet">
+
+    <link href="<?php bloginfo("template_url")?>/style.css" rel="stylesheet">
+  </head>
+<!-- NAVBAR
+================================================== -->
+  <body>
+  
+      <div class="container">
+
+          <div class="logo">
+        <a href="<?php echo get_option("home")?>">
+            <img  class="img-responsive col-lg-12 col-md-12 col-sm-12 col-xs-12" src="<?php echo get_theme_mod("theme_logo_image") ?>">
+        </a>
+      </div>  <!--  end LOGO -->
+
+        <div class="navbar " role="navigation">
+          <div class="container">
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+            
+            </div>
+            <div class="navbar-collapse collapse">
+              <ul class="nav navbar-nav">
+                <li class="active"><a href="#">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#contact">Contact</a></li>
+              
+                 
+              </ul>
+            </div>
+          </div>
+        </div>
+
+     
+    </div>
+ </div>
+
+    <!-- Carousel
+    ================================================== -->
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+      <!-- Indicators -->
+      <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+        <li data-target="#myCarousel" data-slide-to="2"></li>
+      </ol>
+      <div class="carousel-inner" role="listbox">
+        <div class="item active">
+          <img src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="First slide">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>Exampleheadline.</h1>
+              <p>Note: If you're viewing this page via a <code>file://</code> URL, the "next" and "previous" Glyphicon buttons on the left and right might not load/display properly due to web browser security rules.</p>
+              <p><a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p>
+            </div>
+          </div>
+        </div>
+        <div class="item">
+          <img src="data:image/gif;base64,R0lGODlhAQABAIAAAGZmZgAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Second slide">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>Another example headline.</h1>
+              <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+              <p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>
+            </div>
+          </div>
+        </div>
+        <div class="item">
+          <img src="data:image/gif;base64,R0lGODlhAQABAIAAAFVVVQAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Third slide">
+          <div class="container">
+          
+              <h1>One more for good measure.</h1>
+              <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+              <p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
+           
+          </div>
+        </div>
+      </div>
+      <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div><!-- /.carousel -->
+
