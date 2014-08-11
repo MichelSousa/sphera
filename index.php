28e84de00b42c828ff76a789dabb79539c798f32
MichelSousa <michel-damasceno@hotmail.com>
2014-08-11 12:36:16 -0300

    alteracos sections

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
          
         <section id="slide-posts" class="col-lg-12 col-sm-12 col-xs-12 text-center">
 
-            <h1>SPHERA</h1>
+            <h1 class="title-section">SPHERA</h1>
 
             <p>fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris </p>
 
@@ -78,7 +78,7 @@
 
          </section>   <!-- /.END section-stats -->
 
-         <section id="section-quotes" class="col-lg-12 col-sm-12 col-xs-12">
+         <section id="section-quotes" class="col-lg-12 col-sm-12 col-xs-12 full-width">
             
            <div class="container"> 
             <div class="col-lg-5">
@@ -100,6 +100,8 @@
          <div class="container">
 
          <section id="section-team" class="col-lg-12 col-sm-12 col-xs-12 text-center">
+
+            <h1 class="title-section">NOSSA EQUIPE</h1>
 
           <div id="thumbcarousel" class="carousel " data-interval="false">
             <div class="carousel-inner">
@@ -138,44 +140,75 @@
                 </div><!-- /item -->
 
             </div><!-- /carousel-inner -->
-            <a class="left carousel-control" href="#thumbcarousel" role="button" data-slide="prev">
-                <span class="glyphicon glyphicon-chevron-left"></span>
-            </a>
-            <a class="right carousel-control" href="#thumbcarousel" role="button" data-slide="next">
-                <span class="glyphicon glyphicon-chevron-right"></span>
-            </a>
+
+           
 
           </div> <!-- /thumbcarousel -->
+
+           <a class="left carousel-control" href="#thumbcarousel" role="button" data-slide="prev">
+                <span class=" glyphicon-chevron-left"></span>
+            </a>
+            <a class="right carousel-control" href="#thumbcarousel" role="button" data-slide="next">
+                <span class=" glyphicon-chevron-right"></span>
+            </a>
+
+         </div> <!-- /container -->
 
          </div> <!-- /container -->
 
          </section> <!-- /.END section-clients --> 
 
-         <section id="section-differential" class="col-lg-12 col-sm-12 col-xs-12 text-center">
+         <section id="section-differential" class="col-lg-12 col-sm-12 col-xs-12 text-center full-width">
+             <h1 class="title-section">Nosso diferencial</h1>   
+              <div class="container">
+                  <div class="col-lg-4">
 
+                          <img class="" src="<?php bloginfo("template_url")?>/img/img1-differential.png" alt="Generic placeholder image" >
+                          <h2>Heading</h2>
+                          <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+                         
+                   </div><!-- /.col-lg-4 -->
+
+                   <div class="col-lg-4">
+
+                          <img class="" src="<?php bloginfo("template_url")?>/img/img2-differential.png" alt="Generic placeholder image" >
+                          <h2>Heading</h2>
+                          <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+                         
+                   </div><!-- /.col-lg-4 -->
+
+                   <div class="col-lg-4">
+
+                          <img class="" src="<?php bloginfo("template_url")?>/img/img3-differential.png" alt="Generic placeholder image" >
+                          <h2>Heading</h2>
+                          <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+                         
+                   </div><!-- /.col-lg-4 -->
+              </div><!-- /.container-->
          </section>  <!-- /.END section-differential -->
 
          <section id="section-products" class="col-lg-12 col-sm-12 col-xs-12 text-center">
-
+             <h1 class="title-section">Produtos</h1>     
          </section>  <!-- /.END section-products -->
+             
 
          <section id="section-promotions" class="col-lg-12 col-sm-12 col-xs-12 text-center">
-
+             <h1 class="title-section">Promoções</h1>       
          </section>  <!-- /.END section-promotions -->
 
 
          <section id="section-recents" class="col-lg-12 col-sm-12 col-xs-12 text-center">
-
+             <h1 class="title-section">Projetos recentes</h1>       
          </section>  <!-- /.END section-recents -->
 
 
          <section id="section-service" class="col-lg-12 col-sm-12 col-xs-12 text-center">
-
+             <h1 class="title-section">Nossos serviços</h1>       
          </section>  <!-- /.END section-service -->
 
 
         <section id="recent-publications" class="col-lg-12 col-sm-12 col-xs-12 text-center">
-
+             <h1 class="title-section">Publicações recentes</h1>       
         </sction>  <!-- /.END recent-publications -->
 
 
@@ -183,7 +216,7 @@
            
 
          <section id="contact" class="col-lg-12 col-sm-12 col-xs-12 text-center">
-
+             <h1 class="title-section">Entre em contato</h1>       
          </sction>  <!-- /.END contact -->
 
 
